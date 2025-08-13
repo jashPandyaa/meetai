49a56b0bed3832242895e42e61d4570acf8540e2
Jash Pandya <pandyajash2005@gmail.com>
2025-08-13 18:48:52 +0530

    08: TRPC  Setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 
 const page = async () => {
+
   const session = await auth.api.getSession({
     headers : await headers(),
   });
