af99c5194bb6f9ce31a86222e447847e6a06665c
Jash Pandya <pandyajash2005@gmail.com>
2025-08-17 15:50:30 +0530

    15: agent Update & Delete

--- src/modules/agents/ui/components/agents-form.tsx
+++ src/modules/agents/ui/components/agents-form.tsx
@@ -41,6 +41,21 @@ export const AgentForm = ({
                 await queryClient.invalidateQueries(
                     trpc.agents.getMany.queryOptions({}),
                 );
+                //TODO: Invalidate Free tier usage
+                onSuccess?.();
+            },
+            onError : (error) => {
+                toast.error(error.message);
+            },
+        }),
+    );
+
+    const updateAgent = useMutation(
+        trpc.agents.update.mutationOptions({
+            onSuccess : async () => {
+                await queryClient.invalidateQueries(
+                    trpc.agents.getMany.queryOptions({}),
+                );
 
                 if( initialValues?.id ){
                     await queryClient.invalidateQueries(
@@ -64,11 +79,11 @@ export const AgentForm = ({
     });
 
     const isEdit = !!initialValues?.id;
-    const isPending = createAgent.isPending;
+    const isPending = createAgent.isPending || updateAgent.isPending ;
 
     const onSubmit = ( values:z.infer<typeof agentsInsertSchema> ) => {
         if( isEdit ){
-            console.log("TODO : update agent");
+            updateAgent.mutate({ ...values, id: initialValues.id });
         }else{
             createAgent.mutate(values);
         }
